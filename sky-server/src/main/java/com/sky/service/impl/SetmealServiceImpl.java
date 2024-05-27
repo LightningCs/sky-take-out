6203d30e29b070bcbaa4f8502cc42ea643c68f48
86151 <1619159009@qq.com>
2024-05-27 22:11:17 +0800

    redis链接、新增套餐、套餐分页查询

---
+++ sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
@@ -0,0 +1,78 @@
+package com.sky.service.impl;
+
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.sky.constant.MessageConstant;
+import com.sky.dto.SetmealDTO;
+import com.sky.dto.SetmealPageQueryDTO;
+import com.sky.entity.Dish;
+import com.sky.entity.Setmeal;
+import com.sky.entity.SetmealDish;
+import com.sky.mapper.DishMapper;
+import com.sky.mapper.SetmealDishMapper;
+import com.sky.mapper.SetmealMapper;
+import com.sky.result.PageResult;
+import com.sky.service.SetmealService;
+import com.sky.vo.SetmealVO;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class SetmealServiceImpl implements SetmealService {
+
+    @Autowired
+    private DishMapper dishMapper;
+
+    @Autowired
+    private SetmealMapper setmealMapper;
+
+    @Autowired
+    private SetmealDishMapper setmealDishMapper;
+
+    /**
+     * 新增套餐
+     * @param setmealDTO
+     */
+    @Override
+    @Transactional
+    public void saveWithDish(SetmealDTO setmealDTO) {
+
+        List<SetmealDish> setmealDishes = setmealDTO.getSetmealDishes();
+
+        //套餐中包含未起售菜品
+        for (SetmealDish setmealDish : setmealDishes) {
+            Dish dish = dishMapper.getById(setmealDish.getDishId());
+            if (dish.getStatus() == 0) throw new RuntimeException(MessageConstant.SETMEAL_ENABLE_FAILED);
+        }
+
+        //新增套餐
+        Setmeal setmeal = new Setmeal();
+        BeanUtils.copyProperties(setmealDTO, setmeal);
+        setmealMapper.save(setmeal);
+
+        //新增套餐菜品关系表
+        Long setmealId = setmeal.getId();
+        setmealDishes.forEach(setmealDish -> setmealDish.setSetmealId(setmealId));
+
+        setmealDishMapper.insertBatch(setmealDishes);
+    }
+
+    /**
+     * 套餐分页查询
+     * @param setmealPageQueryDTO
+     * @return
+     */
+    @Override
+    public PageResult pageQuery(SetmealPageQueryDTO setmealPageQueryDTO) {
+
+        PageHelper.startPage(setmealPageQueryDTO.getPage(), setmealPageQueryDTO.getPageSize());
+
+        Page<Setmeal> setmeal = setmealMapper.pageQuery(setmealPageQueryDTO);
+
+        return new PageResult(setmeal.getTotal(), setmeal.getResult());
+    }
+}
