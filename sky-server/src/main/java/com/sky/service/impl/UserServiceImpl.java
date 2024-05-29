146fcfef98108c065c434e97708e6d0b12de6b25
86151 <1619159009@qq.com>
2024-05-29 16:13:06 +0800

    用户登录

---
+++ sky-server/src/main/java/com/sky/service/impl/UserServiceImpl.java
@@ -0,0 +1,79 @@
+package com.sky.service.impl;
+
+import com.alibaba.fastjson.JSONObject;
+import com.sky.constant.MessageConstant;
+import com.sky.dto.UserLoginDTO;
+import com.sky.entity.User;
+import com.sky.mapper.UserMapper;
+import com.sky.properties.WeChatProperties;
+import com.sky.service.UserService;
+import com.sky.utils.HttpClientUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private WeChatProperties weChatProperties;
+
+    @Autowired
+    private UserMapper userMapper;
+
+    private static final String URL = "https://api.weixin.qq.com/sns/jscode2session";
+
+    /**
+     * 微信登录
+     * @param userLoginDTO
+     * @return
+     */
+    @Override
+    public User wxLogin(UserLoginDTO userLoginDTO) {
+        String openid = getOpenid(userLoginDTO.getCode());
+
+        //判断openid是否为空
+        if (openid == null) {
+            throw new RuntimeException(MessageConstant.LOGIN_FAILED);
+        }
+
+        //判断当前用户openid是否在user表
+        User user = userMapper.getByOpenid(openid);
+
+        //如果是则新增用户
+        if (user == null) {
+            user = User.builder()
+                    .openid(openid)
+                    .createTime(LocalDateTime.now())
+                    .build();
+            userMapper.insert(user);
+        }
+
+        //返回用户
+        return user;
+    }
+
+    /**
+     * 获取openid
+     * @param code
+     * @return
+     */
+    private String getOpenid(String code) {
+        //调用微信接口服务，获得当前微信用户的openid
+        Map<String, String> map = new HashMap<>();
+        map.put("appid", weChatProperties.getAppid());
+        map.put("secret", weChatProperties.getSecret());
+        map.put("js_code", code);
+        map.put("grant_type", "authorization_code");
+
+        String json = HttpClientUtil.doGet(URL, map);
+
+        JSONObject jsonObject = JSONObject.parseObject(json);
+        String openid = jsonObject.getString("openid");
+
+        return openid;
+    }
+}
